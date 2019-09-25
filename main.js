9d71f768fe546cce44547269e0c93b08cde847b5
Justin Wai <justin.wai.97@gmail.com>
2019-09-25 16:30:51 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,120 @@
+const STORE = [
+    {
+        quesetion: 'Which of these terms means to cook food in its own juices with a small amount of fat over low heat, just until softened?',
+        answers: [
+            'Sweating',
+            'Simmering',
+            'Stewing',
+            'Sautéing',
+        ],
+        correctAnswer:
+            'Sweating',
+
+    },
+    {
+        quesetion:
+            'What is the difference between a convection oven and a conventional oven?',
+        answers: [
+            'A convection oven uses a fan',
+            'A convection oven lacks the broiler setting',
+            'A convection oven uses radioactivity',
+        ],
+        correctAnswer:
+        'What is the difference between a convection oven and a conventional oven?',
+    
+    },
+    {
+        quesetion: 'When it comes to Italian pasta and sauces',
+        answers: [
+            'Long pastas are best with cream sauces or olive oil',
+            'The redder the sauce, the longer the pasta should be',
+            'It’s all good!',
+            'Whole grain pasta is best with red sauce',
+        ],
+        correctAnswer:
+            'Long pastas are best with cream sauces or olive oil',
+
+    },
+    {
+        question: 'What should you look for when buying fresh mussels?',
+        answers: [
+            'Mussels with closed shells or shells that snap shut when tapped',
+            'Mussels with widely opened shells',
+            'Mussels that are light-colored',
+            'Mussels with a while film around the lip of the shell',
+        ],
+        correctAnswer:
+        'Mussels with closed shells or shells that snap shut when tapped',
+
+    },
+    {
+        quesetion: 'Which type of wood should you NOT use for smoking foods on an outdoor grill?',
+        answers: [
+            'Softwoods',
+            'Hardwoods',
+            'Fruitwoods',
+            'Hardwood Chips',
+        ],
+        correctAnswer:
+        'Softwoods',
+
+    },
+    {
+        quesetion: 'Which of these is an 18th-century word for flattening chicken for quick cooking?',
+        answers: [
+            'Spatchcock',
+            'Anvil',
+            'Poundeyre',
+            'Palacing',
+        ],
+        correctAnswer:
+            'Spatchcock',
+    },
+    {
+        quesetion: 'What is a quick fix if your rice is too wet?',
+        answers: [
+            'Uncover the pot',
+            'Turn up the heat',
+            'Stir the Rice',
+            'Add flour',
+        ],
+        correctAnswer: 
+        'Uncover the pot',
+
+    },
+    {
+        quesetion: 'A basic pesto sauce consists of basil, Parmesan, olive oil, salt and'
+        answers: [
+            'Pine nuts',
+            'Pepper',
+            'Tomato Paste',
+
+        ],
+        correctAnswer:
+        'Pine nuts',
+        
+    },
+    {
+        quesetion: 'When a recipe calls for wine, most cooks avoid using cooking wines because they contain what?',
+        answers: [
+            'Salt',
+            'Too much alcohol',
+            'Fat',
+
+        ],
+        correctAnswer:
+        'Salt',
+    },
+    {
+        quesetion: 'Which of the following should you avoid marinating overnight?',
+        answers: [
+            'Fish',
+            'Pork',
+            'Chicken', 
+
+        ],
+        correctAnswer:
+        'Fish',
+    },
+
+]
